2aa355b9f211b56f355f3aa8a1ea880b0ca998e9
yuto2009 <0425-0621@gse.okayama-c.ed.jp>
2025-11-26 14:23:09 +0000

    ｐｈｐ追加

--- test.php
+++ test.php
@@ -1,2 +1,16 @@
 <?php
+try {
+		$db = new PDO('mysql:dbname=sample;host=127.0.0.1;charset=utf8mb4', 'root', '');
+		$db -> exec('insert into profile values(1, "自分の名前", 16, current_timestamp)');
+
+		print '<h1>DB接続成功</h1>';
+
+		$records = $db->query('select * from profile');
+                       while($record = $records->fetch()) {
+                                     print $record['id'] . '<br>';
+                       }
+	} catch(PDOException $e) {
+		print 'DB接続エラー：' . $e->getMessage();
+	}
+
 ?>
