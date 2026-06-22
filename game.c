df07242a0d8bc1a860672bd62122ba4d288ab772
yuto2009 <0425-0621@gse.okayama-c.ed.jp>
2026-06-22 21:37:59 +0900

    あ

--- game.c
+++ game.c
@@ -1,8 +1,8 @@
 // memo
 // 構造体はmain関数の外で宣言する？
 #include <stdio.h>
-#include<time.h>
-#include<stdlib.h>
+#include <time.h>
+#include <stdlib.h>
 // 構造体らしい
 typedef struct
 {
@@ -14,15 +14,16 @@ typedef struct
 // 攻撃関数player
 void attack(Character *attacker, Character *target)
 {
-    int damege = attacker->atk + (rand() % 10); //atk ~ atk + 9random
+    int damege = attacker->atk + (rand() % 10); // atk ~ atk + 9random
     target->hp -= damege;
-    printf("%sに%dのダメージ\n",target->name,damege);
+    printf("%sに%dのダメージ\n", target->name, damege);
 }
 
-void enemy_attack(Character *attacker , Character *target){
-    int damege = attacker->atk + (rand() % 5); //atk ~ atk + 9random
+void enemy_attack(Character *attacker, Character *target)
+{
+    int damege = attacker->atk + (rand() % 5); // atk ~ atk + 9random
     target->hp -= damege;
-    printf("%sに%dのダメージ\n",target->name,damege);
+    printf("%sに%dのダメージ\n", target->name, damege);
 }
 // 表示関数
 void show_status(Character *c)
@@ -39,19 +40,21 @@ int mpattack(Character *mpattacker)
     }
     int mpdamege = mpattacker->atk + (rand() % 20);
     mpattacker->mp -= 10;
-    printf("%dダメージ",mpdamege);
+    printf("%dダメージ", mpdamege);
     return mpdamege;
 }
 
-void enemy_mpattack(Character * attacker, Character *target){
-    if(attacker->mp < 10){
+void enemy_mpattack(Character *attacker, Character *target)
+{
+    if (attacker->mp < 10)
+    {
         enemy_attack(attacker, target);
         return;
     }
     attacker->mp -= 10;
-    int damege = attacker->atk + (rand()% 30 );
+    int damege = attacker->atk + (rand() % 30);
     target->hp -= damege;
-    printf("%sの魔法攻撃%dのダメージ\n", attacker->name,damege);
+    printf("%sの魔法攻撃%dのダメージ\n", attacker->name, damege);
 }
 
 void use_potion(Character *c, int *potion)
@@ -102,9 +105,154 @@ void expup(Character *p, Character *enemy, int *exp, int *level)
     }
     // 追加機能レベルがどこかのラインを超えたら、必要expを上げる
 }
+
+void show_map(char map[5][20])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            printf("%c", map[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void move_player(char map[5][20], int *px, int *py, int dx, int dy)
+{
+    int nx = *px + dx;
+    int ny = *py + dy;
+
+    if (map[ny][nx] == '#')
+    {
+        printf("壁だ\n");
+        return;
+    }
+    map[*py][*px] = ' ';
+    *px += dx;
+    *py += dy;
+    map[*py][*px] = 'p';
+}
+
+void move_enemy(char map[5][20], int *ex, int *ey)
+{
+
+    int dir = rand() % 4;
+
+    int dx = 0;
+    int dy = 0;
+
+    switch (dir)
+    {
+    case 0:
+        dy = -1;
+        break;
+    case 1:
+        dy = 1;
+        break;
+    case 2:
+        dx = -1;
+        break;
+    case 3:
+        dx = 1;
+        break;
+    default:
+        break;
+    }
+
+    int nx = *ex + dx;
+    int ny = *ey + dy;
+
+    if (map[ny][nx] == '#')
+    {
+        return;
+    }
+    map[*ey][*ex] = ' ';
+    *ex = nx;
+    *ey = ny;
+    map[*ey][*ex] = 'e';
+}
+
+int battle(Character *player, Character *mob, int *potion, int *exp, int *level)
+{
+    while (1)
+    {
+        expup(player, mob, exp, level);
+        if (mob->hp <= 0)
+        {
+            // enemy_index++;
+            // if (enemy_index >= 3)
+            //{
+            //     printf("すべての敵を倒した\n");
+            //     printf("gamecleare");
+            //     printf("%dレベルまで到達\n", level);
+            //     break;
+            // }
+            // printf("次の敵が現れた\n");
+            // printf("%sだ\n", enemies[enemy_index].name);
+            printf("%sを倒した\n", mob->name);
+            return 1;
+        }
+
+        if (player->hp <= 0)
+        {
+            printf("gameover\n");
+            printf("%dレベルまで到達\n", level);
+            return 0;
+        }
+
+        int a;
+        printf("攻撃\n");
+        printf("1,2から選んでね\n");
+        printf("1は攻撃、2は魔法、3は回復\n");
+        scanf("%d", &a);
+
+        int turn_atk = player->atk;
+        switch (a)
+        {
+        case 1:
+            attack(player, mob);
+            break;
+        case 2:
+            mob->hp -= mpattack(player);
+            break;
+        case 3:
+            use_potion(player, potion);
+            break;
+        }
+
+        printf("敵からの攻撃\n");
+        int enemy_action = rand() % 2;
+        if (enemy_action == 0)
+        {
+            enemy_attack(mob, player);
+        }
+        else
+        {
+            enemy_mpattack(mob, player);
+        }
+        printf("----------\n");
+        show_status(player);
+        show_status(mob);
+        printf("----------\n");
+    }
+}
+
 int main(void)
 {
     srand(time(NULL));
+    int px = 1, py = 1;
+    int ex = 10, ey = 2;
+    char input, map[5][20] = {
+                    "###################",
+                    "#                 #",
+                    "#                 #",
+                    "#                 #",
+                    "###################",
+                };
+    map[py][px] = 'p';
+    map[ey][ex] = 'e';
+    show_map(map);
     int a, potion = 3, exp = 0, level = 1;
 
     // 順番は上の構造体の宣言の順
@@ -118,56 +266,43 @@ int main(void)
     printf("fight\n");
     while (1)
     {
-        expup(&player, &enemies[enemy_index], &exp, &level);
-        if (enemies[enemy_index].hp <= 0)
+        scanf(" %c", &input);
+        switch (input)
         {
+        case 'w':
+            move_player(map, &px, &py, 0, -1);
+            break;
+        case 's':
+            move_player(map, &px, &py, 0, 1);
+            break;
+        case 'a':
+            move_player(map, &px, &py, -1, 0);
+            break;
+        case 'd':
+            move_player(map, &px, &py, 1, 0);
+            break;
+        case 'q':
+            return 0;
+        }
+        move_enemy(map, &ex, &ey);
+        show_status(&player);
+        show_map(map);
+        if (px == ex && py == ey)
+        {
+            printf("戦闘開始\n");
+            int result = battle(&player, &enemies[enemy_index], &potion, &exp, &level);
+            if (result == 0)
+            {
+                printf("敗北\n");
+                break;
+            }
             enemy_index++;
             if (enemy_index >= 3)
             {
-                printf("すべての敵を倒した\n");
+                printf("gameclear");
                 break;
             }
-            printf("次の敵が現れた\n");
+            ex = 10, ey = 2;
         }
-
-        if (player.hp <= 0)
-        {
-            break;
-        }
-
-        printf("攻撃\n");
-        printf("1,2から選んでね\n");
-        printf("1は攻撃、2は魔法、3は回復\n");
-        scanf("%d", &a);
-
-        int turn_atk = player.atk;
-        switch (a)
-        {
-        case 1:
-            attack(&player, &enemies[enemy_index]);
-            break;
-        case 2:
-            enemies[enemy_index].hp -= turn_atk;
-            break;
-        case 3:
-            use_potion(&player, &potion);
-            break;
-        }
-
-        printf("敵からの攻撃\n");
-        int enemy_action = rand() % 2;
-        if(enemy_action == 0) {
-            enemy_attack (&enemies[enemy_index], &player);
-        } else {
-            enemy_mpattack(&enemies[enemy_index], &player);
-        }
-        enemy_attack(&enemies[enemy_index], &player);
-        printf("----------\n");
-        show_status(&player);
-        show_status(&enemies[enemy_index]);
-        printf("----------\n");
-    
     }
-printf("finish\n");
-return 0;
 }
